33da7608e2b7b19abda5d9af6de038d2a7cf0e75
TheWolfInstinct <fixer3838@live.fr>
2021-05-11 23:36:55 +0200

    ADDED reading of config file and execution of the actix server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+use ruchat_backend::configuration::get_config;
+use ruchat_backend::run::run;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    // Make sure to panic if error while reading conf
+    let configuration = get_config().expect("Failed to read config file");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener)?.await
 }
