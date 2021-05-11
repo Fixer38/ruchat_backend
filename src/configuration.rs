fa50549bf6d9f3635b89364d919337fc967aa830
TheWolfInstinct <fixer3838@live.fr>
2021-05-11 23:50:07 +0200

    ADDED method to get connection string from DatabaseSettings

--- src/configuration.rs
+++ src/configuration.rs
@@ -16,6 +16,15 @@ pub struct DatabaseSettings {
     pub database_name: String,
 }
 
+impl DatabaseSettings {
+    pub fn connection_string(&self) -> String {
+        format!(
+            "postgres://{}:{}@{}:{}/{}",
+            self.username, self.password, self.host, self.port, self.database_name
+        )
+    }
+}
+
 pub fn get_config() -> Result<Settings, config::ConfigError> {
     // Create a default config from the Config module
     let mut settings = config::Config::default();
