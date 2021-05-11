3badae54415b0593dcfcdbb5c31deb3d0beb1405
TheWolfInstinct <fixer3838@live.fr>
2021-05-12 00:37:22 +0200

    ADDED pg_pool to the run function to start the actix server with the proper database pool

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 use std::net::TcpListener;
 use ruchat_backend::configuration::get_config;
 use ruchat_backend::run::run;
+use sqlx::postgres::PgPool;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     // Make sure to panic if error while reading conf
     let configuration = get_config().expect("Failed to read config file");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to postgres database");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    run(listener, connection_pool)?.await
 }
--- src/run.rs
+++ src/run.rs
@@ -1,10 +1,13 @@
-use actix_web::{HttpServer, App};
+use actix_web::{HttpServer, App, web};
 use actix_web::dev::Server;
 use std::net::TcpListener;
+use sqlx::postgres::PgPool;
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
+    let db_pool = web::Data::new(db_pool);
+    let server = HttpServer::new(move || {
         App::new()
+            .app_data(db_pool.clone())
     })
         .listen(listener)?
         .run();
