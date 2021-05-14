1cec13fc083c811a6f8f19df950ef8dc5d01add6
TheWolfInstinct <fixer3838@live.fr>
2021-05-15 00:11:30 +0200

    ADDED configure_database function to create the test database, connect to it and make it available as a pool to execute tests on

--- src/tests/health_check.rs
+++ src/tests/health_check.rs
@@ -1,8 +1,7 @@
 use std::net::TcpListener;
 use crate::run::run;
-use sqlx::PgPool;
-use crate::configuration::get_config;
-use uuid::Uuid;
+use sqlx::{PgPool, PgConnection, Connection, Executor};
+use crate::configuration::{get_config, DatabaseSettings};
 
 pub struct TestApp {
     pub address: String,
@@ -15,10 +14,10 @@ async fn spawn_app() -> TestApp {
     let address = format!("http://127.0.0.1:{}", port);
 
     let mut configuration = get_config().expect("Failed to read config");
-    configuration.database.database_name = Uuid::new_v4().to_string();
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
-        .await
-        .expect("Failed to start the postgres instance");
+    configuration.database.database_name = uuid::Uuid::new_v4().to_string();
+
+    let connection_pool = configure_database(&configuration.database)
+        .await;
 
     let server = run(listener, connection_pool.clone()).expect("Failed to bind address");
     let _ = tokio::spawn(server);
@@ -26,4 +25,24 @@ async fn spawn_app() -> TestApp {
         address,
         db_pool: connection_pool,
     }
+}
+
+pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
+    let mut connection = PgConnection::connect(&config.connection_string_without_db())
+        .await
+        .expect("Failed to connect to Postgres");
+    connection
+        .execute(&*format!(r#"CREATE DATABASE "{}";"#, config.database_name))
+        .await
+        .expect("Failed to create database");
+
+    let connection_pool = PgPool::connect(&config.connection_string())
+        .await
+        .expect("Failed to connect to database");
+    sqlx::migrate!("./migrations")
+        .run(&connection_pool)
+        .await
+        .expect("Failed to migrate the database");
+
+    connection_pool
 }
