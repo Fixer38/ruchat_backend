ef7b90ea23ea52675294899482284b2c03200a98
TheWolfInstinct <fixer3838@live.fr>
2021-05-16 21:53:18 +0200

    ADDED SERIAL datatype for the id field in servers table

--- migrations/20210430190540_create_servers_table.sql
+++ migrations/20210430190540_create_servers_table.sql
@@ -1,6 +1,6 @@
 -- Add migration script here
 CREATE TABLE servers(
-    id uuid NOT NULL,
+    id SERIAL,
     PRIMARY KEY (id),
     name varchar(255) NOT NULL UNIQUE,
     created_at timestamptz NOT NULL
