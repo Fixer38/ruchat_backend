e10bfdaafde3450d8430a12e2150453a784f9d58
TheWolfInstinct <fixer3838@live.fr>
2021-05-18 23:53:14 +0200

    ADDED create route to the app

--- src/run.rs
+++ src/run.rs
@@ -3,12 +3,14 @@ use actix_web::dev::Server;
 use std::net::TcpListener;
 use sqlx::postgres::PgPool;
 use crate::routes::health_check::health_check;
+use crate::routes::server::create;
 
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
     let db_pool = web::Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
+            .route("/create", web::post().to(create))
             .app_data(db_pool.clone())
     })
         .listen(listener)?
