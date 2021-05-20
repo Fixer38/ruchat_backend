7b510adb9dd2884bc5ff7fa6aa902f2aa7ef13c9
TheWolfInstinct <fixer3838@live.fr>
2021-05-20 23:40:13 +0200

    ADDED initialisation of env_logger with init function. If RUST_LOG env variable not set to fallback onto info level logs

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,13 @@ use std::net::TcpListener;
 use ruchat_backend::configuration::get_config;
 use ruchat_backend::run::run;
 use sqlx::postgres::PgPool;
+use env_logger::Env;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    // Init calls set_logger
+    // Printing all logs at info level if RUST_LOG environment variable not set
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     // Make sure to panic if error while reading conf
     let configuration = get_config().expect("Failed to read config file");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
