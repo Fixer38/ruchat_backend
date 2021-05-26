f863c17e6b695ed6ecfaa24b0ae24870dac9e8a4
TheWolfInstinct <fixer3838@live.fr>
2021-05-27 00:55:10 +0200

    MOVED code from server.rs in main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,35 @@ use ruchat_backend::configuration::get_config;
 use ruchat_backend::run::run;
 use sqlx::postgres::PgPool;
 use env_logger::Env;
+use tracing::subscriber::set_global_default;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
+use tracing_log::LogTracer;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // Init calls set_logger
-    // Printing all logs at info level if RUST_LOG environment variable not set
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    // Redirect Actix logs events to the Subscriber
+    LogTracer::init().expect("Failed to set logger");
+
+    // Printing all log spans at info level or above
+    // If RUST_LOG environment variable is not set
+    let env_filter = EnvFilter::try_from_default_env()
+        .unwrap_or_else(|_| EnvFilter::new("info"));
+    let formatting_layer = BunyanFormattingLayer::new(
+        "ruchat_backend".into(),
+        // Output formatted spans to stdout of terminal
+        std::io::stdout
+    );
+
+    // 'With' method provided provided by 'SubscriberExt', extension trait of Subscriber
+    // Given by 'tracing_subscriber'
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
+
+    set_global_default(subscriber).expect("Failed to set Subscriber for log spans");
+
     // Make sure to panic if error while reading conf
     let configuration = get_config().expect("Failed to read config file");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
@@ -16,5 +39,6 @@ async fn main() -> std::io::Result<()> {
         .expect("Failed to connect to postgres database");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener, connection_pool)?.await
+    run(listener, connection_pool)?.await?;
+    Ok(())
 }
