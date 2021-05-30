519585a490c68bb7545477f3d64243c6f25c3636
TheWolfInstinct <fixer3838@live.fr>
2021-05-30 23:45:26 +0200

    ADDED a sink argument to the get_subscriber method to decide where logs should be written

--- src/telemetry.rs
+++ src/telemetry.rs
@@ -1,21 +1,23 @@
 use tracing::subscriber::set_global_default;
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
+use tracing_subscriber::fmt::MakeWriter;
 use tracing_log::LogTracer;
 use tracing::Subscriber;
 
 // Using 'impl Subscriber' as return type instead of complicated Subscriber type
 // Need for Send and Sync to be implemented to use 'init_subscriber' due to its async features
-pub fn get_subscriber(name: String, env_filter: String) -> impl Subscriber + Send + Sync {
+// Can take a sink as argument -> Place we write log to
+pub fn get_subscriber(name: String, env_filter: String, sink: impl MakeWriter + Send + Sync + 'static) -> impl Subscriber + Send + Sync {
     // Printing all log spans at info level or above in case of failure
     // env_filter should be info if the logs have to be at info level
     let env_filter = EnvFilter::try_from_default_env()
         .unwrap_or_else(|_| EnvFilter::new(env_filter));
     let formatting_layer = BunyanFormattingLayer::new(
         name,
-        // Output formatted spans to the terminal stdout
+        // Output formatted spans to the sink passed as an argument
         // Could be redirected to a remote location or dumped
-        std::io::stdout
+        sink
     );
     // 'with' method provided by 'SubscriberExt', Extension trait of Subscriber
     // Given by 'tracing_subscriber'
