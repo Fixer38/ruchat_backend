6e7985da8e98ab43873d552b6bcb4eb52a17fbc7
TheWolfInstinct <fixer3838@live.fr>
2021-05-31 23:45:51 +0200

    REFACTORED log in the create function. Moved the instrumentation to the instrument macro.

--- src/routes/server.rs
+++ src/routes/server.rs
@@ -5,22 +5,20 @@ use uuid::Uuid;
 use serde::Deserialize;
 use tracing_futures::Instrument;
 
-
 #[derive(Deserialize)]
 pub struct FormData {
     name: String,
 }
 
-pub async fn create(form: web::Json<FormData>, pool: web::Data<PgPool>) -> Result<HttpResponse, Error> {
-    let request_id = Uuid::new_v4();
-    // Info span creates a span at the info level
-    let request_span = tracing::info_span!(
-        "Adding a new Server.",
-        %request_id,
+#[tracing::instrument(
+    name = "Creating a new Server",
+    skip(form, pool),
+    fields(
+        request_id = %Uuid::new_v4(),
         name = %form.name
-    );
-    // Enter the request span returns an instance of Entered = a guard
-    let _request_span = request_span.enter();
+    )
+)]
+pub async fn create(form: web::Json<FormData>, pool: web::Data<PgPool>) -> Result<HttpResponse, Error> {
     let query_span = tracing::info_span!("Adding new Server to the database");
     sqlx::query!(
     r#"
@@ -35,9 +33,8 @@ pub async fn create(form: web::Json<FormData>, pool: web::Data<PgPool>) -> Resul
         .await
         .map_err(|e| {
             // Using :? Debug format for deeper error messages
-            tracing::error!("Request_id {} - Failed to execute query: {:?}", request_id, e);
+            tracing::error!("Failed to execute query: {:?}", e);
             error::ErrorInternalServerError("Error From Server when executing Request")
         })?;
-    tracing::info!("Request_id {} - New server details have been saved", request_id);
     Ok(HttpResponse::Ok().finish())
 }
